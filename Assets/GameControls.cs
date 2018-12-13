ea118b8dd977b5f53f129e38e1194ee6f6ecc952
ssmith180 <scs@exitsinc.com>
2018-12-13 14:58:12 -0600

    Scenes in progress

---
+++ Assets/GameControls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControls : MonoBehaviour {
+
+    public GameObject blackMoku, whiteMoku;
+
+    public bool isBlacksTurn = true;
+
+    public GameObject[,] stones = new GameObject[19, 19];
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject[] existingStones = GameObject.FindGameObjectsWithTag("Moku");
+        for (int i = 0; i < existingStones.Length; i++)
+        {
+            Vector3 existingStoneLocation = existingStones[i].transform.position;
+            stones[Mathf.RoundToInt(existingStoneLocation.x), Mathf.RoundToInt(existingStoneLocation.y)] = existingStones[i];
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            int bx = Mathf.RoundToInt(mousePosition.x), by = Mathf.RoundToInt(mousePosition.y);
+
+            if (bx >= 0 && bx <= 18 && by >= 0 && by <= 18 && stones[bx, by] == null)
+            {
+                GameObject newMoku = GameObject.Instantiate(isBlacksTurn ? blackMoku : whiteMoku);
+                newMoku.transform.position = new Vector3(bx, by, 0);
+
+                stones[bx, by] = newMoku;
+
+                RemoveCapturedStones();
+
+                isBlacksTurn = !isBlacksTurn;
+            }
+        }
+	}
+
+    private void RemoveCapturedStones()
+    {
+        // TODO: Fill out this function
+    }
+}
