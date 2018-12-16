35cbb5d3074a00c1fa3de034b32a6ac8b9d6528f
ssmith180 <scs@exitsinc.com>
2018-12-16 06:33:45 -0600

    Performant

--- Assets/GameControls.cs
+++ Assets/GameControls.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 
 public class GameControls : MonoBehaviour {
 
-    public GameObject blackMoku, whiteMoku;
+    public GameObject blackMokuTemplate, whiteMokuTemplate;
 
     public bool isBlacksTurn = true;
 
-    public GameObject[,] stones = new GameObject[19, 19];
     private Vector2Int U = Vector2Int.up, D = Vector2Int.down, L = Vector2Int.left, R = Vector2Int.right;
+
+    private Dictionary<Vector2Int, GameObject> blackMoku = new Dictionary<Vector2Int, GameObject>();
+    private Dictionary<Vector2Int, GameObject> whiteMoku = new Dictionary<Vector2Int, GameObject>();
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +21,9 @@ public class GameControls : MonoBehaviour {
         for (int i = 0; i < existingStones.Length; i++)
         {
             Vector3 existingStoneLocation = existingStones[i].transform.position;
-            stones[Mathf.RoundToInt(existingStoneLocation.x), Mathf.RoundToInt(existingStoneLocation.y)] = existingStones[i];
+            Vector2Int boardPos = new Vector2Int(Mathf.RoundToInt(existingStoneLocation.x), Mathf.RoundToInt(existingStoneLocation.y));
+
+            (existingStones[i].name.StartsWith("B") ? blackMoku : whiteMoku).Add(boardPos, existingStones[i]);
         }
 
 	}
@@ -30,16 +34,18 @@ public class GameControls : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            int bx = Mathf.RoundToInt(mousePosition.x), by = Mathf.RoundToInt(mousePosition.y);
+            Vector2Int p = new Vector2Int(Mathf.RoundToInt(mousePosition.x), Mathf.RoundToInt(mousePosition.y));
 
-            if (bx >= 0 && bx <= 18 && by >= 0 && by <= 18 && stones[bx, by] == null)
+            if (ValidAndEmpty(p))
             {
-                GameObject newMoku = GameObject.Instantiate(isBlacksTurn ? blackMoku : whiteMoku);
-                newMoku.transform.position = new Vector3(bx, by, 0);
+                GameObject newMoku = GameObject.Instantiate(isBlacksTurn ? blackMokuTemplate : whiteMokuTemplate);
+                newMoku.transform.position = new Vector3(p.x, p.y, 0);
 
-                stones[bx, by] = newMoku;
+                (isBlacksTurn ? blackMoku : whiteMoku).Add(p, newMoku);
 
+                DateTime removalStart = DateTime.Now;
                 RemoveCapturedStones();
+                print("Removal in " + (DateTime.Now - removalStart).TotalMilliseconds + " ms");
 
                 isBlacksTurn = !isBlacksTurn;
             }
@@ -48,25 +54,20 @@ public class GameControls : MonoBehaviour {
 
     private void RemoveCapturedStones()
     {
-        GameObject[] allMoku = GameObject.FindGameObjectsWithTag("Moku");
-
-        List<GameObject> opponentMoku = new List<GameObject>();
-        string opponentPrefix = isBlacksTurn ? "White" : "Black";
-        foreach (GameObject moku in allMoku) if (moku.name.StartsWith(opponentPrefix)) opponentMoku.Add(moku);
-
+        Dictionary<Vector2Int, GameObject> opponentMoku = new Dictionary<Vector2Int, GameObject>(), source = (isBlacksTurn ? whiteMoku : blackMoku);
+        foreach (Vector2Int p in source.Keys) opponentMoku.Add(p, source[p]);
         while (opponentMoku.Count > 0)
         {
             MokuGroup group = new MokuGroup(opponentMoku);
 
             bool groupCanBreathe = false;
-            foreach (GameObject moku in group.mokuInGroup) if (MokuCanBreathe(moku)) groupCanBreathe = true;
+            foreach (GameObject moku in group.mokuInGroup.Values) if (MokuCanBreathe(moku)) groupCanBreathe = true;
 
             if (!groupCanBreathe)
-                foreach (GameObject moku in group.mokuInGroup)
+                foreach (Vector2Int p in group.mokuInGroup.Keys)
                 {
-                    Destroy(moku);
-                    Vector2Int pos = new Vector2Int(Mathf.RoundToInt(moku.transform.position.x), Mathf.RoundToInt(moku.transform.position.y));
-                    stones[pos.x, pos.y] = null;
+                    Destroy(group.mokuInGroup[p]);
+                    source.Remove(p);
                 }
         }
     }
@@ -78,36 +79,49 @@ public class GameControls : MonoBehaviour {
 
     bool ValidAndEmpty(Vector2Int v)
     {
-        return (v.x >= 0 && v.x <= 18 && v.y >= 0 && v.y <= 18 && stones[v.x, v.y] == null);
+        return (v.x >= 0 && v.x <= 18 && v.y >= 0 && v.y <= 18 && !blackMoku.ContainsKey(v) && !whiteMoku.ContainsKey(v));
     }
 }
 
 class MokuGroup
 {
-    public List<GameObject> mokuInGroup = new List<GameObject>();
-    private List<GameObject> matchingMoku;
+    public Dictionary<Vector2Int, GameObject> mokuInGroup = new Dictionary<Vector2Int, GameObject>();
+    private Dictionary<Vector2Int, GameObject> matchingMoku;
 
-    public MokuGroup(List<GameObject> matchingMoku)
+    public MokuGroup(Dictionary<Vector2Int, GameObject> matchingMoku)
     {
         this.matchingMoku = matchingMoku;
-        RecursiveGrow(matchingMoku[0]);
+        // Little hacky, but how else do we pick a starting point?
+        GameObject start = null;
+        foreach (GameObject m in matchingMoku.Values)
+        {
+            start = m;
+            break;
+        }
+        RecursiveGrow(start);
+    }
+
+    public MokuGroup(Dictionary<Vector2Int, GameObject> matchingMoku, Vector2Int startingPoint)
+    {
+        this.matchingMoku = matchingMoku;
+        RecursiveGrow(matchingMoku[startingPoint]);
     }
 
     public void RecursiveGrow(GameObject firstMoku)
     {
         if (firstMoku == null) return;
-        mokuInGroup.Add(firstMoku);
-        matchingMoku.Remove(firstMoku);
+        mokuInGroup.Add(MokuPos(firstMoku), firstMoku);
+        matchingMoku.Remove(MokuPos(firstMoku));
 
-        RecursiveGrow(FindMatchingMoku(firstMoku, MokuPos(firstMoku) + Vector2Int.up));
-        RecursiveGrow(FindMatchingMoku(firstMoku, MokuPos(firstMoku) + Vector2Int.down));
-        RecursiveGrow(FindMatchingMoku(firstMoku, MokuPos(firstMoku) + Vector2Int.left));
-        RecursiveGrow(FindMatchingMoku(firstMoku, MokuPos(firstMoku) + Vector2Int.right));
+        RecursiveGrow(FindMatchingMoku(MokuPos(firstMoku) + Vector2Int.up));
+        RecursiveGrow(FindMatchingMoku(MokuPos(firstMoku) + Vector2Int.down));
+        RecursiveGrow(FindMatchingMoku(MokuPos(firstMoku) + Vector2Int.left));
+        RecursiveGrow(FindMatchingMoku(MokuPos(firstMoku) + Vector2Int.right));
     }
 
-    GameObject FindMatchingMoku(GameObject originalMoku, Vector2Int location)
+    GameObject FindMatchingMoku(Vector2Int location)
     {
-        foreach (GameObject searchedMoku in matchingMoku) if (MokuPos(searchedMoku) == location) return searchedMoku;
+        if (matchingMoku.ContainsKey(location)) return matchingMoku[location];
         return null;
     }
 
